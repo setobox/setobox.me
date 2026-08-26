c6ba6ab2e3d532084c3184bd25046def2f233e36
setobox <setoboxes@gmail.com>
2026-08-27 00:51:10 +0800

    feat: add particles background

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,11 +5,28 @@ import HeroSection from "@/components/sections/HeroSection.vue";
 import MarqueeDivider from "@/components/sections/MarqueeDivider.vue";
 import ProjectsSection from "@/components/sections/ProjectsSection.vue";
 import StackSection from "@/components/sections/StackSection.vue";
+import Particles from "@/components/bits/Particles.vue";
 
 defineProps<{ booted: boolean }>();
 </script>
 
 <template>
+  <div fixed top-0 left-0 right-0 bottom-0 z-[-1] pointer-events-none>
+    <Particles
+      class="w-full h-full"
+      :particle-count="200"
+      :particle-spread="13"
+      :speed="0.2"
+      :particle-colors="['#C1A5FF', '#fff']"
+      :move-particles-on-hover="false"
+      :particle-hover-factor="0.5"
+      :alpha-particles="true"
+      :particle-base-size="100"
+      :size-randomness="1"
+      :camera-distance="20"
+      :disable-rotation="true"
+    />
+  </div>
   <div>
     <HeroSection :ready="booted" />
     <MarqueeDivider :texts="['CREATING // DESIGNING', 'EXPLORING \\\\ DISCOVERING']" />
