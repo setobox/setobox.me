b4d37badee5809bc717ddea64bd46f5d1d5f5557
setobox <setoboxes@gmail.com>
2026-08-28 02:30:29 +0800

    fix: route transition
    Vue transition not support multiple root dom

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -11,23 +11,25 @@ defineProps<{ booted: boolean }>();
 </script>
 
 <template>
-  <div fixed top-0 left-0 right-0 bottom-0 z-[-1] pointer-events-none>
-    <Particles
-      class="w-full h-full"
-      :particle-count="200"
-      :particle-spread="13"
-      :speed="0.2"
-      :particle-colors="['#C1A5FF', '#fff']"
-      :move-particles-on-hover="false"
-      :particle-hover-factor="0.5"
-      :alpha-particles="true"
-      :particle-base-size="100"
-      :size-randomness="1"
-      :camera-distance="20"
-      :disable-rotation="true"
-    />
-  </div>
   <div>
+    <!-- A routed component inside <Transition> must expose one element root. -->
+    <div fixed top-0 left-0 right-0 bottom-0 z-[-1] pointer-events-none>
+      <Particles
+        class="w-full h-full"
+        :particle-count="200"
+        :particle-spread="13"
+        :speed="0.2"
+        :particle-colors="['#C1A5FF', '#fff']"
+        :move-particles-on-hover="false"
+        :particle-hover-factor="0.5"
+        :alpha-particles="true"
+        :particle-base-size="100"
+        :size-randomness="1"
+        :camera-distance="20"
+        :disable-rotation="true"
+      />
+    </div>
+
     <HeroSection :ready="booted" />
     <MarqueeDivider :texts="['CREATING // DESIGNING', 'EXPLORING \\\\ DISCOVERING']" />
     <AboutSection :intro="false" />
